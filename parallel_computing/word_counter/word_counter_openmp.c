0b340569447cb8db3f26b93d7453288b1a39b96b
gorgonun <polariusvulcano@gmail.com>
2021-08-06 15:19:10 -0300

    implemented

--- parallel_computing/word_counter/word_counter_openmp.c
+++ parallel_computing/word_counter/word_counter_openmp.c
@@ -10,14 +10,27 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    // it assumes no word exceeds length of 1023
-    char word[1024];
     int occurences[argc - 2];
 
-    // this for does not need parallelism
     for(int i = 0; i < argc - 1; i++) {
         occurences[i] = 0;
     }
+
+    // it assumes no word exceeds length of 1023
+    char word[1024];
+
+    char * buffer = 0;
+    long length;
+    FILE * f = fopen (argv[1], "rb");
+
+    fseek(f, 0, SEEK_END);
+    length = ftell(f);
+    fseek(f, 0, SEEK_SET);
+
+    buffer = malloc(length + 1);
+    fread(buffer, 1, length, f);
+    buffer[length] = 0;
+    fclose(f);
 
     // this is a very expensive operation that iterates
     // over the text argc - 2 times
@@ -25,9 +38,10 @@ int main(int argc, char *argv[]) {
     // version with the openmp version
     #pragma omp parallel for private(word)
     for(int i = 2; i < argc; i++) {
-        FILE *file = fopen(argv[1], "r");
-    
-        while (fscanf(file, " %1023s", word) == 1) {
+        int offset;
+        char *ptr = buffer;
+        while(sscanf(ptr, "%1023s%n", word, &offset) == 1) {
+            ptr += offset;
             if(strcmp(argv[i], word) == 0) {
                 occurences[i - 2]++;
             }
